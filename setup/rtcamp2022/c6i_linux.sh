be9c07c3a6390e22b2df26410848be0c5cd61250
demotomohiro <gpuppur@gmail.com>
2022-08-22 12:01:39 +0900

    Install ffmpeg on linux instance

--- setup/rtcamp2022/c6i_linux.sh
+++ setup/rtcamp2022/c6i_linux.sh
@@ -3,3 +3,9 @@ yum update -y
 # Python 3.7.10がインストール済み
 yum install -y git tmux
 su ec2-user -c 'git clone https://github.com/demotomohiro/rcauto.git /home/ec2-user/rcauto'
+
+# https://ffmpeg.org/
+# ffmpegはリポジトリにないのでyumでインストールできない。
+su ec2-user -c 'wget -P ~ https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz'
+su ec2-user -c 'tar xf ~/ffmpeg-release-amd64-static.tar.xz -C ~'
+su ec2-user -c 'echo export PATH=~/ffmpeg-5.0.1-amd64-static:\$PATH >> ~/.bashrc'
