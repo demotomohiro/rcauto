ccadbd3287a998a9c8a44f0f664f2e1e864db94a
demotomohiro <gpuppur@gmail.com>
2022-08-30 10:24:37 +0900

    animeka.pyがjpgファイルも見るようにした

--- animeka.py
+++ animeka.py
@@ -18,6 +18,7 @@ for i in imageDir.iterdir():
 
     images = list(i.glob('*.png'))
     images.extend(i.glob('*.bmp'))
+    images.extend(i.glob('*.jpg'))
     images.sort()
 
     fpsFilePath = i / "fps.txt"
